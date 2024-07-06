d1188bf9bfddb4ff80cddb7599f890f288740473
username / umberto conedera <u.conedera@gmail.com>
2024-07-06 14:29:31 +0200

    primoCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import AllTheBook from './components/AllTheBook';
+import MyNav from './components/MyNav';
+import Welcome from './components/Welcome';
+import Myfooter from './components/MyFooter'
+
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <MyNav/>
+       <Welcome/>
+       <AllTheBook/>
+      <Myfooter></Myfooter>
     </div>
   );
 }
